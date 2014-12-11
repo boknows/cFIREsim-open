d5c246d4e7263dd8e3630a0ad6e683a252079260
unknown <bolanlr1@ndeuvwv05b01425.gold.rtgold.nima.mil>
2014-12-11 14:09:09 -0500

    Added a basic form data stub for use in basic testing

--- js/cFIREsimOpen.js
+++ js/cFIREsimOpen.js
@@ -1,18 +1,17 @@
-$( document ).ready(function() {
-	$("#runSim").click(function() {
-		var data = {
-			param: "getAll",
-		};
+$(document).ready(function() {
+    $("#runSim").click(function() {
+        var data = {
+            param: "getAll",
+        };
 
-		$.ajax({
-		    url: "getData.php",
-		    data: data,
-		    type: "POST",
-		    dataType: 'JSON',
-		    success: function(e) {
-		        console.log(e);
-		    },
-		});
-	});
+        $.ajax({
+            url: "getData.php",
+            data: data,
+            type: "POST",
+            dataType: 'JSON',
+            success: function(e) {
+                console.log(e);
+            },
+        });
+    });
 });
-
---
+++ js/formData-stub.js
@@ -0,0 +1,44 @@
+var formData = {
+	"retirementStartYear": 2014,
+	"retirementEndYear": 2034,
+
+	"portfolio": {
+		"initial": 1000000,
+		"percentEquities": 75,
+		"percentBonds": 25, 
+		"percentGold": 0,
+		"percentCash": 0,
+		"growthOfCash": 0.25,
+		"rebalanceAnnually": true
+	},
+	
+	"spending": {
+		"initial": 40000,
+		"method": "inflationAdjusted" 
+	},
+
+	"extraIncome": {
+		"socialSecurity": {
+			"val": 0,
+			"startYear": 2032,
+			"endYear": 2100
+		},
+		"socialSecuritySpouse": {
+			"val": 0,
+			"startYear": 2032,
+			"endYear": 2100
+		}
+	},
+	
+	"extraSpending": [
+		{
+			"label": "",
+			"val": 0,
+			"startYear": 2030,
+			"endYear": 2034,
+			"recurring": true,
+			"inflationAdjusted": true,
+			"inflationType": "CPI"
+		}
+	]
+};
