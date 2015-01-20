add1ddf6eb40f505be86bfa226b5d976f15e902d
unknown <bolanlr1@ndeuvwv04b01196.gold.rtgold.nima.mil>
2015-01-20 14:19:38 -0500

    Completed baseline simulation. Verified results compared to legacy cFIREsim. Interesting note: When changing the cumulative inflation calculation from the legacy version (which calculates inflation of any given year based on next years CPI) to the newer version (which states that in Year 1, inflation is essentially 0, so that the inflation calculation in the final year isn't based on non-existant data)... the baseline failure rate goes from 8 failures out of 114, to 5 failures out of 114.

--- js/cFIREsimOpen.js
+++ js/cFIREsimOpen.js
@@ -7,7 +7,6 @@ $(document).ready(function() {
 var Simulation = {
     sim: [],
     runSimulation: function(form) {
-        console.log(form);
         var startYear = form.retirementStartYear;
         var endYear = form.retirementEndYear;
         var cycleLength = endYear - startYear + 1;
@@ -24,9 +23,11 @@ var Simulation = {
                 this.calcSpending(form, i, j); //Nominal spending for this specific cycle
             	this.calcMarketGains(form, i, j); //Calculate market gains on portfolio based on allocation from form and data points
                 this.calcEndPortfolio(form, i, j); //Sum up ending portfolio
+                
             }
         }
-        console.log(this.sim);
+        this.calcFailures(this.sim);
+        console.log(this.sim[0]);
     },
     cycle: function(startOfRange, endOfRange) {
         //The starting CPI value of this cycle, for comparison throughout the cycle.
@@ -34,11 +35,11 @@ var Simulation = {
         var cyc = [];
 
         for (var year = startOfRange; year < endOfRange; year++) {
-            data = Market[year.toString()]
+            data = Market[year.toString()];
             cyc.push({
                 "year": year,
                 "data": data,
-                "portfolio": {"start": null, "end": null, "infAdjStart": null, "infAdjEnd": null},
+                "portfolio": {"start": null, "end": null, "infAdjStart": null, "infAdjEnd": null, "fees": null},
                 "spending": null,
                 "infAdjSpending": null,
                 "equities": {"growth": null, "val": null},
@@ -54,10 +55,10 @@ var Simulation = {
 
     },
     roundTwoDecimals: function(num) {
-        return Math.round(num * 100) / 100;
+        return Math.ceil(num * 100) / 100;
     },
-    cumulativeInflation: function(startCPI, endCPI) {
-        return 1 + ((endCPI - startCPI) / startCPI)
+    cumulativeInflation: function(startCPI, endCPI) { 
+        return 1+((endCPI-startCPI)/startCPI);
     },
     calcStartPortfolio: function(form, i, j) {
         if (j > 0) {
@@ -79,7 +80,6 @@ var Simulation = {
     },
     calcMarketGains: function(form, i, j){
         var portfolio = this.sim[i][j].portfolio.start;
-
         portfolio = portfolio - this.sim[i][j].spending; //Take out spending before calculating asset allocation. This simulates taking your spending out at the beginning of a year.
 
         //Calculate value of each asset class based on allocation percentages
@@ -91,12 +91,21 @@ var Simulation = {
         //Calculate growth
     	this.sim[i][j].equities.growth = this.roundTwoDecimals(equities * (this.sim[i][j].data.growth));
     	this.sim[i][j].dividends.growth = this.roundTwoDecimals(equities * this.sim[i][j].data.dividends);
-    	this.sim[i][j].bonds.growth = this.roundTwoDecimals(bonds * (this.sim[i][j].data.fixed_income));
+
+        //New Bond Calculation to incorporate capital appreciation. 
+        if (typeof(Market[this.sim[i][j].year + 1]) == "undefined") {
+            this.sim[i][j].bonds.growth = this.roundTwoDecimals(bonds * (this.sim[i][j].data.fixed_income));
+        } else {
+            var bondsGrowth1 = (this.sim[i][j].data.fixed_income) * (1 - (Math.pow((1 + Market[this.sim[i][j].year + 1].fixed_income), (-9)))) / Market[this.sim[i][j].year + 1].fixed_income;
+            var bondsGrowth2 = (1 / (Math.pow((1 + Market[this.sim[i][j].year + 1].fixed_income), 9))) - 1;
+            this.sim[i][j].bonds.growth = this.roundTwoDecimals(bonds * (bondsGrowth1 + bondsGrowth2 + this.sim[i][j].data.fixed_income));
+        }
+
     	this.sim[i][j].gold.growth = this.roundTwoDecimals(gold * (this.sim[i][j].data.gold));
     	this.sim[i][j].cash.growth = this.roundTwoDecimals(cash * ((form.portfolio.growthOfCash/100)));
 
         //Calculate total value
-        this.sim[i][j].equities.val = this.roundTwoDecimals(equities + this.sim[i][j].equities.growth);
+        this.sim[i][j].equities.val = this.roundTwoDecimals(equities + this.sim[i][j].equities.growth + this.sim[i][j].dividends.growth);
         this.sim[i][j].dividends.val = this.sim[i][j].dividends.growth;
         this.sim[i][j].bonds.val = this.roundTwoDecimals(bonds + this.sim[i][j].bonds.growth);
         this.sim[i][j].gold.val = this.roundTwoDecimals(gold + this.sim[i][j].gold.growth);
@@ -105,7 +114,11 @@ var Simulation = {
     },
     calcEndPortfolio: function(form, i, j) {
         if (form.portfolio.rebalanceAnnually == true) {
-            var feesIncurred = (this.sim[i][j].portfolio.start + this.sim[i][j].equities.growth + this.sim[i][j].bonds.growth + this.sim[i][j].cash.growth + this.sim[i][j].gold.growth) * (form.portfolio.percentFees / 100);
+            var feesIncurred = this.roundTwoDecimals((this.sim[i][j].portfolio.start - this.sim[i][j].spending + this.sim[i][j].equities.growth + this.sim[i][j].bonds.growth + this.sim[i][j].cash.growth + this.sim[i][j].gold.growth) * (form.portfolio.percentFees / 100));
+            if(i==0){
+                console.log(this.sim[i][j].portfolio.start, this.sim[i][j].equities.growth, this.sim[i][j].bonds.growth, this.sim[i][j].cash.growth, this.sim[i][j].gold.growth, feesIncurred);
+            }
+            this.sim[i][j].portfolio.fees = feesIncurred;
             var sumOfAdjustments = 0; //Sum of all portfolio adjustments for this given year. SS/Pensions/Extra Income/Extra Spending.
 
             //Calculate current allocation percentages after all market gains are taken into consideration
@@ -115,10 +128,10 @@ var Simulation = {
             var currPercGold = this.sim[i][j].gold.val / (this.sim[i][j].equities.val + this.sim[i][j].bonds.val + this.sim[i][j].cash.val + this.sim[i][j].gold.val);
 
             //Equally distribute fees and portoflio adjustments amongst portfolio based on allocation percentages
-            this.sim[i][j].equities.val = this.sim[i][j].equities.val + (curPercEquities*sumOfAdjustments) - (curPercEquities*feesIncurred);
-            this.sim[i][j].cash.val = this.sim[i][j].cash.val + (currPercCash*sumOfAdjustments) - (currPercCash*feesIncurred);
-            this.sim[i][j].bonds.val = this.sim[i][j].bonds.val + (currPercBonds*sumOfAdjustments) - (currPercBonds*feesIncurred);
-            this.sim[i][j].gold.val = this.sim[i][j].gold.val + (currPercGold*sumOfAdjustments) - (currPercGold*feesIncurred);
+            this.sim[i][j].equities.val = this.roundTwoDecimals(this.sim[i][j].equities.val + (curPercEquities*sumOfAdjustments) - (curPercEquities*feesIncurred));
+            this.sim[i][j].cash.val = this.roundTwoDecimals(this.sim[i][j].cash.val + (currPercCash*sumOfAdjustments) - (currPercCash*feesIncurred));
+            this.sim[i][j].bonds.val = this.roundTwoDecimals(this.sim[i][j].bonds.val + (currPercBonds*sumOfAdjustments) - (currPercBonds*feesIncurred));
+            this.sim[i][j].gold.val = this.roundTwoDecimals(this.sim[i][j].gold.val + (currPercGold*sumOfAdjustments) - (currPercGold*feesIncurred));
 
             //Sum all assets to determine portfolio end value.
             this.sim[i][j].portfolio.end = this.roundTwoDecimals(this.sim[i][j].equities.val + this.sim[i][j].bonds.val + this.sim[i][j].cash.val + this.sim[i][j].gold.val);
@@ -128,5 +141,21 @@ var Simulation = {
 
         }
     },
+    calcFailures: function(results) {
+        var totalFailures = 0;
+        for(var i=0;i<results.length;i++){
+            var cycleFailure = false;
+            for(var j=0;j<results[i].length;j++){
+                if(results[i][j].portfolio.end < 0){
+                    cycleFailure = true;
+                    //console.log("Failed ", i, j, results[i][j])
+                }
+            }
+            if(cycleFailure == true){
+                totalFailures++;
+            }
+        }
+        console.log("Failed " + totalFailures + " out of " + results.length + " cycles.");
+    }
 
 };
