f9150072bd629f43820907b0ae2b2c9b82da2d11
Jonathan Christian <jont.christian@gmail.com>
2015-09-21 15:26:32 -0700

    Fixed a bug causing the VPW method to go negative with a spending floor set and fixed a bug causing the ending portfolio value to sometimes be NaN.

--- js/spendingModule.js
+++ js/spendingModule.js
@@ -133,14 +133,16 @@ var SpendingModule = {
             var ceiling = SpendingModule.calcBasicSpendingCeiling(form, sim, i, j);
 
             var uncappedSpending = -SpendingModule.calcPayment(form.spending.vpwRateOfReturn / 100, yearsLeftInSimulation, sim[i][j].portfolio.start, Number(form.spending.vpwFutureValue));
-            return Math.round(Math.min(Math.max(uncappedSpending, floor), ceiling));
+            var cappedSpending = Math.min(Math.max(uncappedSpending, floor), ceiling);
+
+            return Math.min(Math.max(uncappedSpending, floor), ceiling);
         }
     },
     calcBasicSpendingFloor: function(form, sim, i, j) {
         if(form.spending.floor == 'definedValue' && "floorValue" in form.spending) {
-            return form.spending.floorValue * sim[i][j].cumulativeInflation;
+            return Math.min(form.spending.floorValue * sim[i][j].cumulativeInflation, sim[i][j].portfolio.start);
         } else if (form.spending.floor == "pensions" && sim[i][j].socialSecurityAndPensionAdjustments != null){
-            return sim[i][j].socialSecurityAndPensionAdjustments;
+            return Math.min(sim[i][j].socialSecurityAndPensionAdjustments, sim[i][j].portfolio.start);
         }
         return 0;
     },
